0904089fb51d0b92625e675f0439184409a57238
chenweichuan <stream_26@sina.com>
2021-11-10 15:25:11 +0800

    init code

---
+++ src/main/java/org/springframework/boot/simplespringbootddd/SimpleSpringBootDddApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.simplespringbootddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springframework/boot/simplespringbootddd/SimpleSpringBootDddApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.simplespringbootddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
