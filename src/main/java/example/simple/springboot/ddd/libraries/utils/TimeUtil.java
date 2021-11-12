111eb0d6a6bb30e4900a8e18a6fea78dfa03f13a
chenweichuan <stream_26@sina.com>
2021-11-12 18:35:11 +0800

    友好时间方法

--- src/main/java/example/simple/springboot/ddd/libraries/utils/TimeUtil.java
+++ src/main/java/example/simple/springboot/ddd/libraries/utils/TimeUtil.java
@@ -15,6 +15,12 @@ import java.util.TimeZone;
 public class TimeUtil {
     final private static String TIME_ZONE = "Asia/Shanghai";
 
+    final private static Integer SECONDS_OF_YEAR = 31536000;
+    final private static Integer SECONDS_OF_MONTH = 2592000;
+    final private static Integer SECONDS_OF_DAY = 86400;
+    final private static Integer SECONDS_OF_HOUR = 3600;
+    final private static Integer SECONDS_OF_MINUTE = 60;
+
     public static SimpleDateFormat getSimpleDateFormat() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         simpleDateFormat.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
@@ -47,5 +53,27 @@ public class TimeUtil {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime localDateTime = LocalDateTime.parse(dateTimeStr, formatter);
         return localDateTime.atZone(ZoneId.of(TIME_ZONE)).toEpochSecond();
+    }
+
+    public static String toFriendlyTime(LocalDateTime time, String nowText) {
+        LocalDateTime now = LocalDateTime.now();
+        long diffSeconds = toEpochSecond(now) - toEpochSecond(time);
+        if (diffSeconds > SECONDS_OF_YEAR) {
+            return (int)Math.floor((double) (diffSeconds / SECONDS_OF_YEAR)) + "年前";
+        } else if (diffSeconds > SECONDS_OF_MONTH) {
+            return (int)Math.floor((double) (diffSeconds / SECONDS_OF_MONTH)) + "月前";
+        } else if (diffSeconds > SECONDS_OF_DAY) {
+            return (int)Math.floor((double) (diffSeconds / SECONDS_OF_DAY)) + "天前";
+        } else if (diffSeconds > SECONDS_OF_HOUR) {
+            return (int)Math.floor((double) (diffSeconds / SECONDS_OF_HOUR)) + "小时前";
+        } else if (diffSeconds > SECONDS_OF_MINUTE) {
+            return (int)Math.floor((double) (diffSeconds / SECONDS_OF_MINUTE)) + "分钟前";
+        } else {
+            return nowText;
+        }
+    }
+
+    public static String toFriendlyTime(LocalDateTime localDateTime) {
+        return toFriendlyTime(localDateTime, "刚刚");
     }
 }
