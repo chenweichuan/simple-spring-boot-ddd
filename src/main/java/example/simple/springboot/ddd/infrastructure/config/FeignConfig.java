5a340c8f5f5646bf28c8000aadd8c050f91fa353
chenweichuan <stream_26@sina.com>
2021-12-30 11:52:42 +0800

    feign config 调整

--- src/main/java/example/simple/springboot/ddd/infrastructure/config/FeignConfig.java
+++ src/main/java/example/simple/springboot/ddd/infrastructure/config/FeignConfig.java
@@ -31,19 +31,7 @@ import java.util.regex.Pattern;
 /**
  * @author chenweichuan
  */
-@Configuration
 public class FeignConfig {
-    final private static Pattern ENV_COOKIE_NAME_PATTERN = Pattern.compile("^(passthrough_cookine_name_pattern)$");
-
-    @Bean
-    Client client() {
-        return new OkHttpClient(
-                new okhttp3.OkHttpClient.Builder()
-                        .addInterceptor(new RequestInterceptor())
-                        .build()
-        );
-    }
-
     @Bean
     Encoder encoder() {
         return new SpringEncoder(()-> new HttpMessageConverters(new MappingJackson2HttpMessageConverter(JsonUtil.createObjectMapper())));
@@ -62,33 +50,5 @@ public class FeignConfig {
     @Bean
     Logger.Level loggerLevel() {
         return Logger.Level.FULL;
-    }
-
-    private static class RequestInterceptor implements Interceptor {
-        @Override
-        public @NonNull Response intercept(Chain chain) throws IOException {
-            Request request = chain.request();
-            Request newRequest = request.newBuilder()
-                    .addHeader("Cookie", buildCookieHeaderValue())
-                    .build();
-            return chain.proceed(newRequest);
-        }
-    }
-
-    private static String buildCookieHeaderValue() {
-        Cookie[] cookies=((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest().getCookies();
-        StringBuilder valueBuilder = new StringBuilder();
-        for (Cookie cookie: cookies) {
-            try {
-                if (ENV_COOKIE_NAME_PATTERN.matcher(cookie.getName()).matches()) {
-                    valueBuilder.append(URLEncoder.encode(cookie.getName(), StandardCharsets.UTF_8.toString()));
-                    valueBuilder.append("=");
-                    valueBuilder.append(URLEncoder.encode(cookie.getValue(), StandardCharsets.UTF_8.toString()));
-                    valueBuilder.append(";");
-                }
-            } catch (Exception ignored) {
-            }
-        }
-        return valueBuilder.toString();
     }
 }
